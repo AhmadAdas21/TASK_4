aa43e982954e433ffac9cca36378d20098331720
AhmadAdas21 <ahmadnajiadas20@gmail.com>
2026-09-22 11:59:00 +0300

    the last changes before i implement the tests

--- TASK_4/services.cs
+++ TASK_4/services.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -187,24 +189,160 @@ namespace TASK_4
      public  void add_product_to_order()
         {
             Console.WriteLine("you are in add product to order feature");
+            Console.WriteLine("enter the order id");
+            int order_id;
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(), out order_id))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, please enter a valid order id");
+                }
+            }
+            int product_id;
+            Console.WriteLine("enter the product id");
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(), out product_id))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, please enter a valid product id");
+                }
+            }
+            order selected = orders.FirstOrDefault(o => o.id == order_id);
 
+            if (selected == null)
+            {
+                Console.WriteLine("order not found");
+                return;
+            }
+            product added = products.FirstOrDefault(o => o.id == order_id);
+
+            if (added == null)
+            {
+                Console.WriteLine("product not found");
+                return;
+            }
+            int quant;
+            Console.WriteLine("enter the quantity of product you want");
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(), out quant))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, please enter a valid quantity");
+                }
+            }
+            if (quant > added.quantity)
+            {
+                Console.WriteLine("not enough stock");
+                return;
+            }
+
+            order_item item = new order_item(added.name, quant, added.price);
+            selected.order_items.Add(item);
+            added.quantity -= quant;
 
         }
-       public void view_orders()
+        public void view_orders()
         {
-
+            Console.WriteLine("here i will provide you with orders list");
+            foreach(var o in orders)
+            {
+                Console.WriteLine(o.id + " " + o.customer_id + " " + o.status + " " + o.created_date);
+            }
         }
       public  void search_orders()
         {
-
+            Console.WriteLine("you are in search orders feature");
+            Console.WriteLine("enter the order id");
+            int idd;
+            while (true)
+            {
+                
+                if (int.TryParse(Console.ReadLine(),out idd ))
+                {
+                    Console.WriteLine("valid input");
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, please enter a valid order id");
+                }
+            }
+            orders.Where(x=>x.id==idd).ToList().ForEach(x => Console.WriteLine(x.id + " " + x.customer_id + " " + x.status + " " + x.created_date));
+            Console.WriteLine("thanks for using this feature");
         }
-     public  void filter_orders_by_status()
+        public void filter_orders_by_status()
         {
+            Console.WriteLine("you are in filter orders by status feature");
+            Console.WriteLine("enter 1 if you want to filter completed orders");
+            Console.WriteLine("enter 0 if you want to filter pending orders");
+            int ch;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out ch))
+                {
+                    if (ch == 1)
+                    {
+                        orders.Where(x => x.status == true).ToList().ForEach(x => Console.WriteLine(x.id + " " + x.customer_id + " " + x.status + " " + x.created_date));
+                        break;
+                    }
+                    else if (ch == 0)
+                    {
+                        orders.Where(x => x.status == false).ToList().ForEach(x => Console.WriteLine(x.id + " " + x.customer_id + " " + x.status + " " + x.created_date));
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid input, please enter 1 or 0");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, please enter a valid status");
+                }
+
+            }
+            Console.WriteLine("thanks for using this feature");
 
         }
       public  void calculate_order_total()
         {
-
+            Console.WriteLine("you are in total ordeer feature");
+            Console.WriteLine("enter the id of the order");
+            int idd;
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(),out idd))
+                {
+                    Console.WriteLine("you have entered a valid order id");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, please enter a valid order id");
+                }
+            }
+            if(!orders.Any(o => o.id == idd))
+            {
+                Console.WriteLine("order not found");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("the total cost of the order for this id");
+                Console.WriteLine(idd);
+                Console.WriteLine(orders.FirstOrDefault(o => o.id == idd).order_items.Sum(x => x.unit_price * x.quantity));
+            }
         }
     }
 }
